4532d7ca06227e02ff0d5f418762446a347b12df
AleksandroKravchuk <93790774+AleksandroKravchuk@users.noreply.github.com>
2022-12-02 12:02:59 +0000

    Deploying to gh-pages from @ AleksandroKravchuk/out-light@a136f36c35431d8de75d3ad2f2f5548147128517 🚀

--- static/js/830.f6c60252.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkout_light=self.webpackChunkout_light||[]).push([[830],{6053:function(t,n,r){r.d(n,{nfZ:function(){return o}});var e=r(9983);function o(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",stroke:"#000",strokeWidth:"2",d:"M3,3 L21,21 M3,21 L21,3"}}]})(t)}},2860:function(t,n,r){function e(t){throw new TypeError('"'+t+'" is read-only')}r.d(n,{Z:function(){return e}})}}]);
-//# sourceMappingURL=830.f6c60252.chunk.js.map

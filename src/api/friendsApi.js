d60df6293791b26e2af56015bf6b1176c830a672
Oleksandr <alexandrokrk@gmail.com>
2022-12-02 11:36:35 +0200

    1

--- src/api/friendsApi.js
+++ src/api/friendsApi.js
@@ -3,6 +3,5 @@ import axios from 'axios';
 export const fetchFriends = async url => {
   const { data } = await axios.get(`${url}/friends`);
   const { friends } = data.data;
-  console.log(friends);
   return friends;
 };

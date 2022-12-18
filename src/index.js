079a4842e00debb8ee3a84da12be1f8072660d69
Oleksandr <alexandrokrk@gmail.com>
2022-12-18 10:44:48 +0200

    1

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <PersistGate loading={'Loading...'} persistor={persistor}>
-      <BrowserRouter>
+      <BrowserRouter basename="/out-light/">
         <App />
       </BrowserRouter>
     </PersistGate>
